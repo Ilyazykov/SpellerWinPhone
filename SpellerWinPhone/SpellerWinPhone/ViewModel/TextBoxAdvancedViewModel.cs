20f820ac233d28735643468af459fe5995186fef
Ilyazykov <ilya.zykov@icloud.com>
2014-02-23 02:01:57 +0400

    User Control added

---
+++ SpellerWinPhone/SpellerWinPhone/ViewModel/TextBoxAdvancedViewModel.cs
@@ -0,0 +1,45 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows;
+
+namespace SpellerWinPhone.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class TextBoxAdvancedViewModel : ViewModelBase
+    {
+        public string TempText
+        {
+            get
+            {
+                return "temprorary text"; //TODO - другое что-то надо возвращать
+            }
+        }
+
+        public RelayCommand<string> CheckCommand
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MvvmViewModel1 class.
+        /// </summary>
+        public TextBoxAdvancedViewModel()
+        {
+            CheckCommand = new RelayCommand<string>((msg) => Spellchecking(msg));
+        }
+
+        private object Spellchecking(string msg)
+        {
+            MessageBox.Show("Введённая строка ->" + msg);
+            return null;
+        }
+
+
+    }
+}
--- SpellerWinPhone/SpellerWinPhone/ViewModel/ViewModelLocator.cs
+++ SpellerWinPhone/SpellerWinPhone/ViewModel/ViewModelLocator.cs
@@ -1,17 +1,3 @@
-/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:SpellerWinPhone"
-                           x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
-*/
-
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
@@ -31,18 +17,8 @@ namespace SpellerWinPhone.ViewModel
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
-            ////}
-            ////else
-            ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
-            ////}
-
             SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<TextBoxAdvancedViewModel>();
         }
 
         public MainViewModel Main
@@ -50,6 +26,14 @@ namespace SpellerWinPhone.ViewModel
             get
             {
                 return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        public TextBoxAdvancedViewModel TextBoxAdvance
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<TextBoxAdvancedViewModel>();
             }
         }
         
