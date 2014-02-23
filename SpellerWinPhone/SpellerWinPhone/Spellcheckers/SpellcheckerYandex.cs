18a0031a92e0eb4a53736dbf3a01e1f35cfe3cb5
Ilyazykov <ilya.zykov@icloud.com>
2014-02-23 05:52:42 +0400

    json code getted

--- SpellerWinPhone/SpellerWinPhone/Spellcheckers/SpellcheckerYandex.cs
+++ SpellerWinPhone/SpellerWinPhone/Spellcheckers/SpellcheckerYandex.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows;
 
@@ -10,8 +11,35 @@ namespace SpellerWinPhone.Spellcheckers
     {
         public string findMistakes(string msg)
         {
-            MessageBox.Show(msg);
+            var urlStrin = String.Format("http://speller.yandex.net/services/spellservice.json/checkText?text={0}", msg);
+
+            LoadSiteContent(urlStrin);
+
             return "";
+        }
+
+        public void LoadSiteContent(string url)
+        {
+            WebClient client = new WebClient();
+
+            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(DownloadStringCallback2);
+            client.DownloadStringAsync(new Uri(url));
+        }
+
+        private void DownloadStringCallback2(Object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show("e.Error != null");
+            }
+            // If the request was not canceled and did not throw
+            // an exception, display the resource.
+            else if (!e.Cancelled)
+            {
+                MessageBox.Show((string)e.Result);
+                //If you get the cross-thread exception then use the following line instead of the above
+                //Dispatcher.BeginInvoke(new Action (() => textBlock1.Text = (string)e.Result));
+            }
         }
     }
 }
